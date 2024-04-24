61e6b14c6059f8115c4194a6ea5ee99cc7a9b0ff
mmskazak <mmskazak@gmail.com>
2024-04-24 23:09:24 +0300

    iter1 first commit, http server for create shorted url

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,81 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var urlMap map[string]string
+
+func main() {
+	urlMap = make(map[string]string)
+
+	http.HandleFunc("/", handleHortenUrl)
+	http.HandleFunc("/{id}", handleRedirect)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}
+
+func handleHortenUrl(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Чтение оригинального URL из тела запроса.
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Could not read the body", http.StatusBadRequest)
+		return
+	}
+	originalUrl := string(body)
+
+	// Генерируем уникальный идентификатор для сокращенной ссылки
+	shortUrl := generateShortURL(8)
+	shortedUrl := "http://localhost:8080/" + shortUrl
+	urlMap[shortUrl] = originalUrl
+
+	w.WriteHeader(http.StatusCreated)
+	_, err = fmt.Fprint(w, shortedUrl)
+	if err != nil {
+		return
+	}
+}
+
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	shortUrl := r.URL.Path[1:]
+	originalUrl, ok := urlMap[shortUrl]
+	if !ok {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, originalUrl, http.StatusTemporaryRedirect)
+}
+
+// generateShortURL генерирует случайный строковый идентификатор заданной длины
+func generateShortURL(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, length)
+
+	// Создаем новый генератор случайных чисел
+	src := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(src)
+
+	for i := range b {
+		b[i] = charset[rng.Intn(len(charset))] // генерация случайного индекса
+	}
+	return string(b)
+}
