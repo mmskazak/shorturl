d62a0937a4f032a10c21d2840f776acb224b842d
mmskazak <mmskazak@gmail.com>
2024-04-26 17:48:35 +0300

    added some tests

---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandleRequests_GetUrl_BadRequest(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/wErTdsB", nil)
+	w := httptest.NewRecorder()
+
+	handleRequests(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode, "Ожидаетсся статус код %d, получен %d", http.StatusNotFound, res.StatusCode)
+}
+
+func TestHandleRequests_GetUrl_NotFound(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/wErTdsBk", nil)
+	w := httptest.NewRecorder()
+
+	handleRequests(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusNotFound, res.StatusCode, "Ожидаетсся статус код %d, получен %d", http.StatusNotFound, res.StatusCode)
+}
+
+func TestHandleRequests_GetMainPage(t *testing.T) {
+	//тестовый реквест
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder() //*ResponseRecorder
+
+	handleRequests(w, req)
+
+	//получаем результат *ResponseRecorder
+	res := w.Result()
+	assert.Equal(t, http.StatusOK, res.StatusCode, "Ожидаетсся статус код %d, получен %d", http.StatusOK, res.StatusCode)
+
+	//сразу закрываем чтение
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+
+	require.NoError(t, err, "Ошибка чтения(err) res.Body")
+
+	assert.Equal(t, "Сервис сокращения URL", string(body))
+}
+
+func TestHandleRequests_PostMainPage(t *testing.T) {
+	urlMap = make(map[string]string)
+
+	originalURL := "https://ya.ru"
+	requestBody := bytes.NewBuffer([]byte(originalURL))
+	req := httptest.NewRequest(http.MethodPost, "/", requestBody)
+	w := httptest.NewRecorder()
+
+	handleRequests(w, req)
+
+	res := w.Result()
+
+	assert.Equal(t, http.StatusCreated, res.StatusCode, "Ожидаетсся статус код %d, получен %d", http.StatusCreated, res.StatusCode)
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+
+	require.NoError(t, err, "Ошибка чтения res.Body")
+
+	shortenedURL := string(body)
+	assert.NotEmpty(t, shortenedURL, "Ожидается сокращенный URL")
+
+	// Additional checks if needed, e.g., checking if shortened URL is valid
+}
