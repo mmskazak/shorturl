8ed0ea94aaffcfdc28ccc73b91d662e2e9f550d9
mmskazak <mmskazak@gmail.com>
2024-05-03 12:46:06 +0300

    upd tests

--- internal/app/handlers/handles_test.go
+++ internal/app/handlers/handles_test.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"bytes"
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
@@ -12,7 +13,6 @@ import (
 )
 
 func TestCreateShortURL(t *testing.T) {
-	t.Skip()
 	// Initialize a new MapStorage for testing
 	data := mapstorage.NewMapStorage()
 
@@ -37,14 +37,16 @@ func TestCreateShortURL(t *testing.T) {
 }
 
 func TestHandleRedirect(t *testing.T) {
-	t.Skip()
 	// Initialize a new MapStorage for testing
 	data := *mapstorage.NewMapStorage()
 	err := data.SetShortURL("x5x5x5x5", "https://ya.ru")
 	require.NoError(t, err)
 
-	// Define a test handler function that wraps CreateShortURL
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// Create a new chi router
+	r := chi.NewRouter()
+
+	// Define the route and bind the handler
+	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
 		HandleRedirect(w, r, &data)
 	})
 
@@ -55,7 +57,7 @@ func TestHandleRedirect(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	// Call the handler function with the test request and response recorder
-	handler.ServeHTTP(w, req)
+	r.ServeHTTP(w, req)
 
 	// Check that the response status code is 201 Created
 	require.Equal(t, http.StatusTemporaryRedirect, w.Code)
@@ -63,22 +65,27 @@ func TestHandleRedirect(t *testing.T) {
 }
 
 func TestMainPage(t *testing.T) {
-	t.Skip()
-	type args struct {
-		w   http.ResponseWriter
-		req *http.Request
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Success main page",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			MainPage(tt.args.w, tt.args.req)
-		})
-	}
+	// Create a new chi router
+	r := chi.NewRouter()
+
+	// Define the route and bind the handler
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		MainPage(w, r)
+	})
+
+	// Создаем фейковый HTTP запрос
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	require.NoError(t, err)
+
+	// Create a new response recorder to capture the response from the handler
+	w := httptest.NewRecorder()
+
+	// Call the handler function with the test request and response recorder
+	r.ServeHTTP(w, req)
+
+	// Check that the response status code is 201 Created
+	require.Equal(t, http.StatusOK, w.Code)
+
+	assert.Equal(t, w.Body.String(), "Сервис сокращения URL")
+
 }
