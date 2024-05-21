0265d85369a731f08f207682897d0f1c988359f3
mmskazak <mmskazak@gmail.com>
2024-05-21 17:55:06 +0300

    upd

--- internal/services/rwstorage/rwstorage.go
+++ internal/services/rwstorage/rwstorage.go
@@ -1,1 +1,88 @@
 package rwstorage
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+)
+
+type ShortURLStruct struct {
+	UUID        string `json:"uuid"`
+	ShortURL    string `json:"short_url"`
+	OriginalURL string `json:"original_url"`
+}
+
+type Producer struct {
+	file *os.File
+	// добавляем Writer в Producer
+	writer *bufio.Writer
+}
+
+func NewProducer(filename string) (*Producer, error) {
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Producer{
+		file: file,
+		// создаём новый Writer
+		writer: bufio.NewWriter(file),
+	}, nil
+}
+
+func (p *Producer) WriteData(shData *ShortURLStruct) error {
+	data, err := json.Marshal(&shData)
+	if err != nil {
+		return err
+	}
+
+	// записываем событие в буфер
+	if _, err := p.writer.Write(data); err != nil {
+		return err
+	}
+
+	// добавляем перенос строки
+	if err := p.writer.WriteByte('\n'); err != nil {
+		return err
+	}
+
+	// записываем буфер в файл
+	return p.writer.Flush()
+}
+
+type Consumer struct {
+	file *os.File
+	// добавляем reader в Consumer
+	reader *bufio.Reader
+}
+
+func NewConsumer(filename string) (*Consumer, error) {
+	file, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		file: file,
+		// создаём новый Reader
+		reader: bufio.NewReader(file),
+	}, nil
+}
+
+func (c *Consumer) ReadEvent() (*ShortURLStruct, error) {
+	// читаем данные до символа переноса строки
+	data, err := c.reader.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	// преобразуем данные из JSON-представления в структуру
+	event := ShortURLStruct{}
+	err = json.Unmarshal(data, &event)
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
