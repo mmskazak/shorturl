2ad7c0937dde3d0cd28d3888402bf5cf0c4b8bea
mmskazak <mmskazak@gmail.com>
2024-05-21 17:52:25 +0300

    учусь сохранять данные в файл

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"mmskazak/shorturl/internal/app"
 	"mmskazak/shorturl/internal/config"
 	"mmskazak/shorturl/internal/logger"
+	"mmskazak/shorturl/internal/services/rwstorage"
 	"mmskazak/shorturl/internal/storage/mapstorage"
 	"net/http"
 )
@@ -27,6 +29,31 @@ func main() {
 	}
 
 	ms := mapstorage.NewMapStorage()
+
+	filename := "short-url-db.json"
+	// Чтение содержимого файла
+	consumer, _ := rwstorage.NewConsumer(filename)
+
+	// Читаем события в цикле
+	for {
+		dataOfURL, err := consumer.ReadEvent()
+		if err != nil {
+			if err.Error() != "EOF" {
+				fmt.Printf("Ошибка при чтении события: %v\n", err)
+				break
+			}
+			fmt.Println("Достигнут конец файла.")
+			break
+		}
+
+		// Обрабатываем прочитанное событие
+		fmt.Printf("Прочитанны данные: %+v\n", dataOfURL)
+		err = ms.SetShortURL(dataOfURL.ShortURL, dataOfURL.OriginalURL)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
 
 	newApp := app.NewApp(cfg, ms, cfg.ReadTimeout, cfg.WriteTimeout)
 
