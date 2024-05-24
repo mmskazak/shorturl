7e5de7b41228e8ddd6eab776b0537c91ad9ffb3d
mmskazak <mmskazak@gmail.com>
2024-05-24 20:59:05 +0300

    before review

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -11,6 +11,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 const (
@@ -20,68 +23,82 @@ const (
 func main() {
 	cfg, err := config.InitConfig()
 	if err != nil {
-		logger.Logf.Fatalf("ошибка инициализации конфигурации в main %v", err)
+		logger.Logf.Fatalf("Ошибка инициализации конфигурации: %v", err)
 	}
 
 	level, err := cfg.LogLevel.Value()
 	if err != nil {
-		logger.Logf.Warn("ошибка получения уровня логирония %v", err)
+		logger.Logf.Warnf("Ошибка получения уровня логирования: %v", err)
 	}
 
-	_, err = logger.InitWriteToOutput(level)
+	if err := initLoggers(level); err != nil {
+		logger.Logf.Fatalf("Ошибка инициализации логеров: %v", err)
+	}
+
+	ms, err := initializeStorage(cfg, logger.Logf)
 	if err != nil {
-		logger.Logf.Fatalf("ошибка инициализации логера output в main %v", err)
-	}
-
-	_, err = logger.InitWriteToFile(level)
-	if err != nil {
-		logger.Logf.Fatalf("ошибка инициализации логера file в main %v", err)
-	}
-
-	var ms *mapstorage.MapStorage
-	pathToStorage := cfg.FileStoragePath
-	if pathToStorage != "" {
-		logger.Logf.Debug(pathToStorage)
-		// filepath.Split возвращает пару (директория, имя файла), но нам нужно только директорию
-		directoryPath, _ := filepath.Split(pathToStorage)
-		err = os.MkdirAll(directoryPath, os.FileMode(PermFile0750))
-		if err != nil {
-			logger.Logf.Fatalf("ошибка создания дирректории для файла хранилища %v", err)
-		}
-
-		consumer, err := rwstorage.NewConsumer(pathToStorage)
-		if err != nil {
-			logger.Logf.Fatalf("ошибка создания консьюмера для чтения из хранилища")
-		}
-
-		ms = mapstorage.NewMapStorage(cfg.FileStoragePath)
-
-		// Читаем события в цикле
-		for {
-			dataOfURL, err := consumer.ReadDataFromFile()
-			if err != nil {
-				if err.Error() != "EOF" {
-					fmt.Printf("Ошибка при чтении: %v\n", err)
-					break
-				}
-				fmt.Println("Достигнут конец файла.")
-				break
-			}
-
-			// Обрабатываем прочитанное событие
-			fmt.Printf("Прочитанны данные: %+v\n", dataOfURL)
-			ms.Data[dataOfURL.ShortURL] = dataOfURL.OriginalURL
-			fmt.Printf("длинна мапы: %+v\n", len(ms.Data))
-		}
-	} else {
-		ms = mapstorage.NewMapStorage(cfg.FileStoragePath)
+		logger.Logf.Fatalf("Ошибка инициализации хранилища: %v", err)
 	}
 
 	newApp := app.NewApp(cfg, ms, cfg.ReadTimeout, cfg.WriteTimeout)
 
 	if err := newApp.Start(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		logger.Logf.Fatalf("server error: %v", err)
+		logger.Logf.Fatalf("Ошибка сервера: %v", err)
 	}
 
-	logger.Logf.Info("Application has shut down gracefully.")
+	logger.Logf.Info("Приложение завершило работу.")
+}
+
+func initLoggers(level zapcore.Level) error {
+	if _, err := logger.InitWriteToOutput(level); err != nil {
+		return fmt.Errorf("ошибка инициализации логера output: %w", err)
+	}
+	if _, err := logger.InitWriteToFile(level); err != nil {
+		return fmt.Errorf("ошибка инициализации логера file: %w", err)
+	}
+	return nil
+}
+
+func initializeStorage(cfg *config.Config, logf *zap.SugaredLogger) (*mapstorage.MapStorage, error) {
+	pathToStorage := cfg.FileStoragePath
+	var ms *mapstorage.MapStorage
+
+	if pathToStorage == "" {
+		return mapstorage.NewMapStorage(pathToStorage), nil
+	}
+
+	logf.Debug(pathToStorage)
+	if err := os.MkdirAll(filepath.Dir(pathToStorage), os.FileMode(PermFile0750)); err != nil {
+		return nil, fmt.Errorf("ошибка создания директории для файла хранилища: %w", err)
+	}
+
+	consumer, err := rwstorage.NewConsumer(pathToStorage)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка создания консьюмера для чтения из хранилища: %w", err)
+	}
+
+	ms = mapstorage.NewMapStorage(pathToStorage)
+	if err := readFileStorage(consumer, ms, logf); err != nil {
+		return nil, fmt.Errorf("error read storage data: %w", err)
+	}
+
+	return ms, nil
+}
+
+func readFileStorage(consumer *rwstorage.Consumer, ms *mapstorage.MapStorage, logf *zap.SugaredLogger) error {
+	for {
+		dataOfURL, err := consumer.ReadDataFromFile()
+		if err != nil {
+			if err.Error() != "EOF" {
+				return fmt.Errorf("ошибка при чтении: %w", err)
+			}
+			fmt.Println("Достигнут конец файла.")
+			break
+		}
+
+		logf.Infof("Прочитанные данные: %+v\n", dataOfURL)
+		ms.Data[dataOfURL.ShortURL] = dataOfURL.OriginalURL
+		logf.Infof("Длина мапы: %+v\n", len(ms.Data))
+	}
+	return nil
 }
