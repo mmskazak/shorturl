5cffc4f9c4f3b79169d057c2508f0566924610a1
mmskazak <mmskazak@gmail.com>
2024-05-31 13:40:06 +0300

    upd

--- internal/storage/postgresql/postrgresql.go
+++ internal/storage/postgresql/postrgresql.go
@@ -3,7 +3,6 @@ package postgresql
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"mmskazak/shorturl/internal/config"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -14,44 +13,8 @@ type PostgreSQL struct {
 }
 
 func NewPostgreSQL(cfg *config.Config) (*PostgreSQL, error) {
-	// Подключение к базе данных postgres для управления базами данных
-	db, err := sql.Open("pgx", cfg.DataBaseDSN)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open connection to PostgreSQL: %w", err)
-	}
-
-	// Проверяем соединение
-	err = db.Ping()
-	if err != nil {
-		err := db.Close()
-		if err != nil {
-			return nil, fmt.Errorf("failed to close PostgreSQL connection after db.Ping: %w", err)
-		}
-		return nil, fmt.Errorf("failed to ping PostgreSQL connection: %w", err)
-	}
-
-	// Проверяем наличие базы данных dbshorturl и создаем ее при необходимости
-	var dbExists bool
-	err = db.QueryRow("SELECT EXISTS (SELECT datname FROM pg_catalog.pg_database WHERE datname = 'dbshorturl')").Scan(&dbExists) //nolint:lll //так удобнее читать
-	if err != nil {
-		return nil, fmt.Errorf("failed to check if database exists: %w", err)
-	}
-
-	if !dbExists {
-		_, err = db.Exec("CREATE DATABASE dbshorturl")
-		if err != nil {
-			return nil, fmt.Errorf("failed to create database: %w", err)
-		}
-	}
-
-	// Закрываем соединение с базой данных postgres
-	err = db.Close()
-	if err != nil {
-		return nil, fmt.Errorf("failed to close PostgreSQL connection: %w", err)
-	}
-
 	// Подключаемся к базе данных dbshorturl
-	dbShortURL, err := sql.Open("pgx", cfg.DataBaseDSN+" dbname=dbshorturl")
+	dbShortURL, err := sql.Open("pgx", cfg.DataBaseDSN)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open connection to dbshorturl: %w", err)
 	}
@@ -64,18 +27,19 @@ func NewPostgreSQL(cfg *config.Config) (*PostgreSQL, error) {
 
 	// Создаем таблицу shorturl, если она не существует
 	_, err = dbShortURL.Exec(`
-		CREATE TABLE IF NOT EXISTS shorturl (
-			id SERIAL PRIMARY KEY,
-			short_url VARCHAR(10) NOT NULL,
-			original_url TEXT NOT NULL
-		)
-	`)
+			CREATE TABLE IF NOT EXISTS shorturl (
+				id SERIAL PRIMARY KEY,
+				short_url VARCHAR(10) NOT NULL,
+				original_url TEXT NOT NULL
+			)
+		`)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to create table shorturl: %w", err)
 	}
 
 	return &PostgreSQL{
-		dbShortURL,
+		db: dbShortURL,
 	}, nil
 }
 
@@ -109,12 +73,15 @@ func (pg *PostgreSQL) Ping() error {
 	return nil
 }
 
-func (pg *PostgreSQL) Close() {
-	if pg.db != nil {
-		err := pg.db.Close()
-		if err != nil {
-			log.Fatalf("Error closing database connection: %v\n", err)
-		}
-		log.Println("Database connection closed.")
+func (pg *PostgreSQL) Close() error {
+	if pg.db == nil {
+		return nil
 	}
+
+	err := pg.db.Close()
+	if err != nil {
+		return fmt.Errorf("error closing database connection: %w", err)
+	}
+
+	return nil
 }
