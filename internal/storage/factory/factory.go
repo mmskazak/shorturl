c823f861606095c0ab898be48d664c41971d94b1
mmskazak <mmskazak@gmail.com>
2024-06-12 16:02:33 +0300

    upd

--- internal/storage/factory/factory.go
+++ internal/storage/factory/factory.go
@@ -15,22 +15,22 @@ import (
 func NewStorage(ctx context.Context, cfg *config.Config, zapLog *zap.SugaredLogger) (storage.Storage, error) {
 	switch {
 	case cfg.DataBaseDSN != "":
-		PostgreSQL := zapLog.With(zap.String("storage", "PostgreSQL"))
-		pg, err := postgresql.NewPostgreSQL(ctx, cfg, PostgreSQL)
+		pqLogger := zapLog.With(zap.String("storage", "PostgreSQL"))
+		pg, err := postgresql.NewPostgreSQL(ctx, cfg, pqLogger)
 		if err != nil {
 			return nil, fmt.Errorf("failed to connect to postgresql: %w", err)
 		}
 		return pg, nil
 	case cfg.FileStoragePath == "":
-		inMemoryLog := zapLog.With(zap.String("storage", "InMemory"))
-		sm, err := inmemory.NewInMemory(inMemoryLog)
+		inMemoryLogger := zapLog.With(zap.String("storage", "InMemory"))
+		sm, err := inmemory.NewInMemory(inMemoryLogger)
 		if err != nil {
 			return nil, fmt.Errorf("failed to initialize an in-memory store: %w", err)
 		}
 		return sm, nil
 	default:
-		InFile := zapLog.With(zap.String("storage", "InFile"))
-		sf, err := infile.NewInFile(ctx, cfg, InFile)
+		inFileLogger := zapLog.With(zap.String("storage", "InFile"))
+		sf, err := infile.NewInFile(ctx, cfg, inFileLogger)
 		if err != nil {
 			return nil, fmt.Errorf("failed to initialize an in-file store: %w", err)
 		}
