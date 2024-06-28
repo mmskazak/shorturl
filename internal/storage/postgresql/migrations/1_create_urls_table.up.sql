99f43c568164af4557eb6a7cfc3be355f431ad6e
mmskazak <mmskazak@gmail.com>
2024-06-28 16:46:13 +0300

    upd migrations

--- internal/storage/postgresql/migrations/1_create_urls_table.up.sql
+++ internal/storage/postgresql/migrations/1_create_urls_table.up.sql
@@ -1,13 +1,15 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS urls(
-                                         id SERIAL PRIMARY KEY,
-                                         short_url VARCHAR(255) NOT NULL,
-                                         original_url TEXT NOT NULL,
-                                         user_id VARCHAR(255) NOT NULL,
-                                         deleted BOOLEAN default FALSE
+CREATE TABLE IF NOT EXISTS urls (
+                                    id SERIAL PRIMARY KEY,
+                                    short_url VARCHAR(255) NOT NULL,
+                                    original_url TEXT NOT NULL,
+                                    user_id VARCHAR(255) NOT NULL,
+                                    deleted BOOLEAN DEFAULT FALSE,
+                                    CONSTRAINT unique_short_url UNIQUE (short_url),
+                                    CONSTRAINT unique_original_url UNIQUE (original_url)
 );
-CREATE UNIQUE INDEX idx_unique_short_url ON urls(short_url) WHERE deleted = FALSE;
-CREATE UNIQUE INDEX idx_unique_original_url ON urls(original_url) WHERE deleted = FALSE;
 
 COMMIT;
+
+
