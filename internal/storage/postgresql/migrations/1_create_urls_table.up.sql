ab8839232a4cab4106c64f172b100c09a94a393e
mmskazak <mmskazak@gmail.com>
2024-06-28 17:06:19 +0300

    upd migrations 3

--- internal/storage/postgresql/migrations/1_create_urls_table.up.sql
+++ internal/storage/postgresql/migrations/1_create_urls_table.up.sql
@@ -1,15 +1,13 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS short_urls (
-                                          id SERIAL PRIMARY KEY,
-                                          short_url VARCHAR(255) NOT NULL,
-                                          original_url TEXT NOT NULL,
-                                          user_id VARCHAR(255) NOT NULL,
-                                          deleted BOOLEAN DEFAULT FALSE
+CREATE TABLE IF NOT EXISTS urls(
+                                         id SERIAL PRIMARY KEY,
+                                         short_url VARCHAR(255) NOT NULL,
+                                         original_url TEXT NOT NULL,
+                                         user_id VARCHAR(255) NOT NULL,
+                                         deleted BOOLEAN default FALSE
 );
-
--- Создание частичных индексов для уникальности short_url и original_url только для неудаленных записей
-CREATE UNIQUE INDEX idx_unique_short_url ON short_urls(short_url) WHERE deleted = FALSE;
-CREATE UNIQUE INDEX idx_unique_original_url ON short_urls(original_url) WHERE deleted = FALSE;
+-- Создаем индекс, ограничивающий уникальность original_url только для строк, где deleted = FALSE
+CREATE UNIQUE INDEX unique_original_url ON urls (original_url) WHERE deleted = FALSE;
 
 COMMIT;
