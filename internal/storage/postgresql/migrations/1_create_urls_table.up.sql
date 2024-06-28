53ff862cbabd8f47cb110cf84738093e3728a51d
mmskazak <mmskazak@gmail.com>
2024-06-28 16:54:23 +0300

    upd migrations 2

--- internal/storage/postgresql/migrations/1_create_urls_table.up.sql
+++ internal/storage/postgresql/migrations/1_create_urls_table.up.sql
@@ -1,15 +1,15 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS urls (
-                                    id SERIAL PRIMARY KEY,
-                                    short_url VARCHAR(255) NOT NULL,
-                                    original_url TEXT NOT NULL,
-                                    user_id VARCHAR(255) NOT NULL,
-                                    deleted BOOLEAN DEFAULT FALSE,
-                                    CONSTRAINT unique_short_url UNIQUE (short_url),
-                                    CONSTRAINT unique_original_url UNIQUE (original_url)
+CREATE TABLE IF NOT EXISTS short_urls (
+                                          id SERIAL PRIMARY KEY,
+                                          short_url VARCHAR(255) NOT NULL,
+                                          original_url TEXT NOT NULL,
+                                          user_id VARCHAR(255) NOT NULL,
+                                          deleted BOOLEAN DEFAULT FALSE
 );
 
+-- Создание частичных индексов для уникальности short_url и original_url только для неудаленных записей
+CREATE UNIQUE INDEX idx_unique_short_url ON short_urls(short_url) WHERE deleted = FALSE;
+CREATE UNIQUE INDEX idx_unique_original_url ON short_urls(original_url) WHERE deleted = FALSE;
+
 COMMIT;
-
-
