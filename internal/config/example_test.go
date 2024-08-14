d7596494ca6e176e8187b7f3c85e357b65b5bd79
mmskazak <mmskazak@gmail.com>
2024-08-14 13:38:50 +0300

    fix by linter example_test for struct config

--- internal/config/example_test.go
+++ internal/config/example_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-// ExampleConfig демонстрирует пример базовой(по умолчанию) структуры Config.
+// ExampleStructConfig демонстрирует пример базовой(по умолчанию) структуры Config.
 func ExampleStructConfig() {
 	config := &Config{
 		Address:         ":8080",
@@ -18,7 +18,38 @@ func ExampleStructConfig() {
 		DataBaseDSN:     "user:password@/dbname",
 	}
 
-	fmt.Printf("%+v\n", config)
+	// Форматируем вывод в человекочитаемом виде
+	output := fmt.Sprintf(
+		`{
+	Address: %q,
+	BaseHost: %q,
+	LogLevel: %q,
+	ReadTimeout: %v,
+	WriteTimeout: %v,
+	FileStoragePath: %q,
+	SecretKey: %q,
+	DataBaseDSN: %q
+}`,
+		config.Address,
+		config.BaseHost,
+		config.LogLevel,
+		config.ReadTimeout,
+		config.WriteTimeout,
+		config.FileStoragePath,
+		config.SecretKey,
+		config.DataBaseDSN,
+	)
+
+	fmt.Println(output)
 	// Output:
-	// &{Address::8080 BaseHost:http://localhost:8080 FileStoragePath:/tmp/short-url-db.json DataBaseDSN:user:password@/dbname SecretKey:secret LogLevel:info ReadTimeout:10s WriteTimeout:10s}
+	// {
+	//	Address: ":8080",
+	//	BaseHost: "http://localhost:8080",
+	//	LogLevel: "info",
+	//	ReadTimeout: 10s,
+	//	WriteTimeout: 10s,
+	//	FileStoragePath: "/tmp/short-url-db.json",
+	//	SecretKey: "secret",
+	//	DataBaseDSN: "user:password@/dbname"
+	// }
 }
