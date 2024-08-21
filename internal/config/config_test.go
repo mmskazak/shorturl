f35a15f5194d772809d3f811d138c2ffb02fb013
mmskazak <mmskazak@gmail.com>
2024-08-21 17:40:00 +0300

    add tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"go.uber.org/zap/zapcore"
 	"testing"
 	"time"
 
@@ -34,6 +35,83 @@ func TestInitConfig(t *testing.T) {
 
 			if !cmp.Equal(got, tt.want) {
 				t.Errorf("InitConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLogLevel_Value(t *testing.T) {
+	tests := []struct {
+		name    string
+		ll      LogLevel
+		want    zapcore.Level
+		wantErr bool
+	}{
+		{
+			name:    "debug level",
+			ll:      "debug",
+			want:    zapcore.DebugLevel,
+			wantErr: false,
+		},
+		{
+			name:    "info level",
+			ll:      "info",
+			want:    zapcore.InfoLevel,
+			wantErr: false,
+		},
+		{
+			name:    "warn level",
+			ll:      "warn",
+			want:    zapcore.WarnLevel,
+			wantErr: false,
+		},
+		{
+			name:    "error level",
+			ll:      "error",
+			want:    zapcore.ErrorLevel,
+			wantErr: false,
+		},
+		{
+			name:    "dpanic level",
+			ll:      "dpanic",
+			want:    zapcore.DPanicLevel,
+			wantErr: false,
+		},
+		{
+			name:    "panic level",
+			ll:      "panic",
+			want:    zapcore.PanicLevel,
+			wantErr: false,
+		},
+		{
+			name:    "fatal level",
+			ll:      "fatal",
+			want:    zapcore.FatalLevel,
+			wantErr: false,
+		},
+		{
+			name:    "unknown level",
+			ll:      "unknown",
+			want:    zapcore.DebugLevel,
+			wantErr: true,
+		},
+		{
+			name:    "empty level",
+			ll:      "",
+			want:    zapcore.DebugLevel,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.ll.Value()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Value() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Value() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
