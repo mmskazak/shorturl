4f83f4c5b11fde2f7ca5b4f21c047fba12c46565
mmskazak <mmskazak@gmail.com>
2024-08-24 19:07:22 +0300

    upd tests

--- internal/handlers/api/handle_create_short_url_test.go
+++ internal/handlers/api/handle_create_short_url_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"mmskazak/shorturl/internal/contracts/mocks"
 	"mmskazak/shorturl/internal/services/shorturlservice"
@@ -180,4 +181,39 @@ func TestHandleCreateShortURL_ErrConflict(t *testing.T) {
 	assert.Equal(t, http.StatusConflict, w.Code)
 	fmt.Println(w.Body.String())
 	assert.NotEmpty(t, w.Body.String())
+}
+
+func TestHandleCreateShortURL_ErrGenerateShortURL(t *testing.T) {
+	// Создание нового контроллера
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// Создание контекста с PayloadJWT
+	ctx := context.WithValue(context.Background(), ctxkeys.PayLoad, jwtbuilder.PayloadJWT{UserID: "11111"})
+
+	// Создание логгера
+	zapSugar := zaptest.NewLogger(t).Sugar()
+
+	// Создание HTTP-запроса и ResponseRecorder
+	w := httptest.NewRecorder()
+	// Создание HTTP-запроса с телом запроса
+	body := strings.NewReader(`{"url": "https://google.ru"}`)
+	req := httptest.NewRequest(http.MethodPost, "/", body)
+	req = req.WithContext(ctx)
+
+	// Создание мока для ISetShortURL
+	data := mocks.NewMockISetShortURL(ctrl)
+
+	// Базовый хост
+	baseHost := "http://localhost"
+
+	shortURLService := mocks.NewMockIShortURLService(ctrl)
+	shortURLService.EXPECT().GenerateShortURL(context.Background(), gomock.Any(), gomock.Any(), data).
+		Return("", errors.New("test error"))
+
+	// Вызов функции
+	HandleCreateShortURL(context.Background(), w, req, data, baseHost, zapSugar, shortURLService)
+
+	// Проверка кода ответа
+	assert.Equal(t, http.StatusInternalServerError, w.Code)
 }
