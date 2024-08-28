bc509ee1baca9418eeac8ae24a12ff9e99ac0806
mmskazak <mmskazak@gmail.com>
2024-08-28 16:26:08 +0300

    upd iter19

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -1,9 +1,12 @@
 package staticlint
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/alexkohler/nakedret"
 	"github.com/kisielk/errcheck/errcheck"
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/buildtag"
 	"golang.org/x/tools/go/analysis/passes/copylock"
 	"golang.org/x/tools/go/analysis/passes/fieldalignment"
@@ -15,11 +18,13 @@ import (
 	"golang.org/x/tools/go/analysis/passes/sortslice"
 	"golang.org/x/tools/go/analysis/passes/structtag"
 	"golang.org/x/tools/go/analysis/passes/testinggoroutine"
-	"mmskazak/shorturl/cmd/staticlint/noosexit"
-	"strings"
-
-	"golang.org/x/tools/go/analysis/multichecker"
+	"honnef.co/go/tools/quickfix"
+	"honnef.co/go/tools/simple"
 	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck"
+	"log"
+	"mmskazak/shorturl/cmd/staticlint/noosexit"
+	"os"
 )
 
 // Допустимое количество строк в функции для возврата голого ответа
@@ -42,23 +47,42 @@ func main() {
 		// ...другие анализаторы по необходимости
 	}
 
+	// определяем map подключаемых правил
+	//checks := map[string]bool{
+	//	"S1001":  true,
+	//	"ST1001": true,
+	//	"QF1002": true,
+	//}
+
+	// Загружаем конфигурацию
+	checks, err := loadConfig("config.json")
+	if err != nil {
+		fmt.Println("Ошибка загрузки конфигурации:", err)
+		return
+	}
+
+	var myChecks []*analysis.Analyzer
+
 	// Анализаторы из пакета staticcheck
 	for _, v := range staticcheck.Analyzers {
-		// Все анализаторы класса SA пакета staticcheck.io
-		if strings.HasPrefix(v.Analyzer.Name, "SA") {
-			analyzers = append(analyzers, v.Analyzer)
+		if checks[v.Analyzer.Name] {
+			myChecks = append(myChecks, v.Analyzer)
 		}
 	}
 
-	// определяем map подключаемых правил
-	checks := map[string]bool{
-		"S1001":  true,
-		"ST1001": true,
-		"QF1002": true,
+	for _, v := range simple.Analyzers {
+		if checks[v.Analyzer.Name] {
+			myChecks = append(myChecks, v.Analyzer)
+		}
 	}
-	var myChecks []*analysis.Analyzer
-	for _, v := range staticcheck.Analyzers {
-		// добавляем в массив нужные проверки
+
+	for _, v := range stylecheck.Analyzers {
+		if checks[v.Analyzer.Name] {
+			myChecks = append(myChecks, v.Analyzer)
+		}
+	}
+
+	for _, v := range quickfix.Analyzers {
 		if checks[v.Analyzer.Name] {
 			myChecks = append(myChecks, v.Analyzer)
 		}
@@ -74,4 +98,30 @@ func main() {
 	analyzers = append(analyzers, noosexit.Analyzer)
 
 	multichecker.Main(analyzers...)
+}
+
+type Config struct {
+	Checks map[string]bool `json:"checks"`
+}
+
+// Считываем конфигурацию из файла
+func loadConfig(path string) (map[string]bool, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось открыть файл конфигурации: %v", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Printf("Error file config not close: %v", err)
+		}
+	}(file)
+
+	var config Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, fmt.Errorf("не удалось декодировать файл конфигурации: %v", err)
+	}
+
+	return config.Checks, nil
 }
