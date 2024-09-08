7171121d59279e53300b980dcdcdc848655aa6da
mmskazak <mmskazak@gmail.com>
2024-09-08 19:49:46 +0300

    upd tests

--- internal/storage/postgresql/delete_urls_test.go
+++ internal/storage/postgresql/delete_urls_test.go
@@ -2,13 +2,14 @@ package postgresql
 
 import (
 	"context"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"mmskazak/shorturl/internal/storage/postgresql/mocks"
 	"testing"
 )
 
-func TestPostgreSQL_DeleteURLs(t *testing.T) {
+func TestPostgreSQL_DeleteURLs_EmptyURLs(t *testing.T) {
 	mockPool := new(mocks.MockDatabase)
 	ctx := context.Background()
 	var urlIDs []string
@@ -18,4 +19,24 @@ func TestPostgreSQL_DeleteURLs(t *testing.T) {
 	}
 	err := s.DeleteURLs(ctx, urlIDs)
 	assert.Equal(t, nil, err)
+}
+
+func TestPostgreSQL_DeleteURLs_ErrBegin(t *testing.T) {
+	mockPool := new(mocks.MockDatabase)
+	mockTx := new(mocks.MockTx)
+	ctx := context.Background()
+
+	urlIDs := []string{
+		"testtest",
+		"test1234",
+		"1234test",
+	}
+
+	s := &PostgreSQL{
+		pool:   mockPool,
+		zapLog: zap.NewNop().Sugar(),
+	}
+	mockPool.On("Begin", ctx).Return(mockTx, errors.New("test error"))
+	err := s.DeleteURLs(ctx, urlIDs)
+	assert.Equal(t, "failed to begin transaction: test error", err.Error())
 }
