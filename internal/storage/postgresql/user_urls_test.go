fdf9e3a407a944b8cfed7b7c9bddb7dacbbfb831
mmskazak <mmskazak@gmail.com>
2024-09-08 14:31:47 +0300

    upd tests

--- internal/storage/postgresql/user_urls_test.go
+++ internal/storage/postgresql/user_urls_test.go
@@ -2,16 +2,16 @@ package postgresql
 
 import (
 	"context"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	"mmskazak/shorturl/internal/models"
 	"mmskazak/shorturl/internal/storage/postgresql/mocks"
 	"testing"
 )
 
 func TestPostgreSQL_GetUserURLs(t *testing.T) {
-	t.Skip()
 	// Создаем моки
 	mockPool := new(mocks.MockDatabase)
 	mockTx := new(mocks.MockTx)
@@ -31,31 +31,13 @@ func TestPostgreSQL_GetUserURLs(t *testing.T) {
 	// Устанавливаем ожидаемые вызовы методов моков
 	mockPool.On("Begin", ctx).Return(mockTx, nil).Once()
 	mockTx.On("Rollback", ctx).Return(nil)
-	mockTx.On("Query", ctx, mock.Anything, []interface{}{userID}).Return(mockRows, nil).Once()
-	mockRows.On("Next").Return(true).Once() // Будем возвращать одну строку
-	mockRows.On("Scan", mock.AnythingOfType("*string"), mock.AnythingOfType("*string")).
-		Run(func(args mock.Arguments) {
-			shortURLPtr := args.Get(0).(*string)
-			*shortURLPtr = "/short"
-			originalURLPtr := args.Get(1).(*string)
-			*originalURLPtr = "http://example.com/original"
-		}).Return(nil)
-	mockRows.On("Next").Return(false)
-	mockRows.On("Close").Return(nil)
-	mockRows.On("Err").Return(nil)
-	mockTx.On("Commit", ctx).Return(nil)
+	mockTx.On("Query", ctx, mock.Anything, []interface{}{userID}).
+		Return(mockRows, errors.New("test error")).Once()
 
 	// Вызываем тестируемую функцию
 	got, err := s.GetUserURLs(ctx, userID, baseHost)
-
-	// Проверяем ошибки
-	require.NoError(t, err)
-	require.Len(t, got, 1, "Expected 1 URL, got %d", len(got))
-
-	// Проверяем корректность полученного URL
-	expectedShortURL := "http://localhost/short"
-	assert.Equal(t, expectedShortURL, got[0].ShortURL)
-	assert.Equal(t, "http://example.com/original", got[0].OriginalURL)
+	assert.Error(t, err)
+	assert.Equal(t, []models.URL(nil), got)
 
 	// Проверяем вызовы методов моков
 	mockPool.AssertExpectations(t)
