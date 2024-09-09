dde5016247b30e95625c19d2356a107de89334fd
mmskazak <mmskazak@gmail.com>
2024-09-09 16:52:47 +0300

    upd tests

--- internal/storage/postgresql/delete_urls_test.go
+++ internal/storage/postgresql/delete_urls_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
 	"mmskazak/shorturl/internal/storage/postgresql/mocks"
 	"testing"
@@ -39,4 +40,29 @@ func TestPostgreSQL_DeleteURLs_ErrBegin(t *testing.T) {
 	mockPool.On("Begin", ctx).Return(mockTx, errors.New("test error"))
 	err := s.DeleteURLs(ctx, urlIDs)
 	assert.Equal(t, "failed to begin transaction: test error", err.Error())
+}
+
+func TestPostgreSQL_DeleteURLs(t *testing.T) {
+	mockPool := new(mocks.MockDatabase)
+	mockTx := new(mocks.MockTx)
+	mockBatchResults := new(mocks.MockBatchResults)
+	ctx := context.Background()
+
+	urlIDs := []string{
+		"testtest",
+		"test1234",
+		"1234test",
+	}
+
+	s := &PostgreSQL{
+		pool:   mockPool,
+		zapLog: zap.NewNop().Sugar(),
+	}
+	mockPool.On("Begin", ctx).Return(mockTx, nil)
+	mockTx.On("Rollback", ctx).Return(nil)
+	mockTx.On("SendBatch", ctx, mock.Anything).Return(mockBatchResults)
+	mockBatchResults.On("Close").Return(nil)
+	mockTx.On("Commit", ctx).Return(nil)
+	err := s.DeleteURLs(ctx, urlIDs)
+	assert.NoError(t, err)
 }
