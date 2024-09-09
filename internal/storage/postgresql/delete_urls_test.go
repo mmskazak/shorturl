12c572289812eb3507663be80e0bc69635557e02
mmskazak <mmskazak@gmail.com>
2024-09-09 17:41:28 +0300

    upd tests

--- internal/storage/postgresql/delete_urls_test.go
+++ internal/storage/postgresql/delete_urls_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"mmskazak/shorturl/internal/storage/postgresql/mocks"
 	"testing"
@@ -42,7 +43,7 @@ func TestPostgreSQL_DeleteURLs_ErrBegin(t *testing.T) {
 	assert.Equal(t, "failed to begin transaction: test error", err.Error())
 }
 
-func TestPostgreSQL_DeleteURLs(t *testing.T) {
+func TestPostgreSQL_DeleteURLs_Success(t *testing.T) {
 	mockPool := new(mocks.MockDatabase)
 	mockTx := new(mocks.MockTx)
 	mockBatchResults := new(mocks.MockBatchResults)
@@ -65,4 +66,56 @@ func TestPostgreSQL_DeleteURLs(t *testing.T) {
 	mockTx.On("Commit", ctx).Return(nil)
 	err := s.DeleteURLs(ctx, urlIDs)
 	assert.NoError(t, err)
+}
+
+func TestPostgreSQL_DeleteURLs_CommitError(t *testing.T) {
+	mockPool := new(mocks.MockDatabase)
+	mockTx := new(mocks.MockTx)
+	mockBatchResults := new(mocks.MockBatchResults)
+	ctx := context.Background()
+
+	urlIDs := []string{
+		"testtest",
+		"test1234",
+		"1234test",
+	}
+
+	s := &PostgreSQL{
+		pool:   mockPool,
+		zapLog: zap.NewNop().Sugar(),
+	}
+	mockPool.On("Begin", ctx).Return(mockTx, nil)
+	mockTx.On("Rollback", ctx).Return(nil)
+	mockTx.On("SendBatch", ctx, mock.Anything).Return(mockBatchResults)
+	mockBatchResults.On("Close").Return(nil)
+	mockTx.On("Commit", ctx).Return(errors.New("test error"))
+	err := s.DeleteURLs(ctx, urlIDs)
+	require.Error(t, err)
+	assert.EqualError(t, err, "failed to commit transaction: test error")
+}
+
+func TestPostgreSQL_DeleteURLs_CloseError(t *testing.T) {
+	mockPool := new(mocks.MockDatabase)
+	mockTx := new(mocks.MockTx)
+	mockBatchResults := new(mocks.MockBatchResults)
+	ctx := context.Background()
+
+	urlIDs := []string{
+		"testtest",
+		"test1234",
+		"1234test",
+	}
+
+	s := &PostgreSQL{
+		pool:   mockPool,
+		zapLog: zap.NewNop().Sugar(),
+	}
+	mockPool.On("Begin", ctx).Return(mockTx, nil)
+	mockTx.On("Rollback", ctx).Return(nil)
+	mockTx.On("SendBatch", ctx, mock.Anything).Return(mockBatchResults)
+	mockBatchResults.On("Close").Return(errors.New("test error"))
+
+	err := s.DeleteURLs(ctx, urlIDs)
+	require.Error(t, err)
+	assert.EqualError(t, err, "failed to delete URLs in batch: test error")
 }
