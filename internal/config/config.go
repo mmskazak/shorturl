f180ade3a5c791cf54157971da82cfdafe91b432
mmskazak <mmskazak@gmail.com>
2024-09-12 23:27:54 +0300

    upd

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -16,14 +17,15 @@ import (
 
 // Config содержит поля конфигурации.
 type Config struct {
-	Address         string        `validate:"required"`  // Адрес сервера
-	BaseHost        string        `validate:"required"`  // Базовый URL
-	FileStoragePath string        `validate:"omitempty"` // Путь к файлу хранилища
-	DataBaseDSN     string        `validate:"omitempty"` // Строка подключения к базе данных
-	SecretKey       string        `validate:"omitempty"` // Секретный ключ для авторизации JWT токена
-	LogLevel        LogLevel      `validate:"required"`  // Уровень логирования
-	ReadTimeout     time.Duration `validate:"required"`  // Таймаут чтения HTTP-запросов
-	WriteTimeout    time.Duration `validate:"required"`  // Таймаут записи HTTP-ответов
+	Address         string        `json:"address" validate:"required"`            // Адрес сервера
+	BaseHost        string        `json:"base_host" validate:"required"`          // Базовый URL
+	FileStoragePath string        `json:"file_storage_path" validate:"omitempty"` // Путь к файлу хранилища
+	DataBaseDSN     string        `json:"database_dsn" validate:"omitempty"`      // Строка подключения к базе данных
+	SecretKey       string        `json:"secret_key" validate:"omitempty"`        // Секретный ключ для авторизации JWT токена
+	LogLevel        LogLevel      `json:"log_level" validate:"required"`          // Уровень логирования
+	ReadTimeout     time.Duration `json:"read_timeout" validate:"required"`       // Таймаут чтения HTTP-запросов
+	WriteTimeout    time.Duration `json:"write_timeout" validate:"required"`      // Таймаут записи HTTP-ответов
+	ConfigPath      string        `json:"config_path" validate:"omitempty"`       // Путь к конфигурационному файлу
 }
 
 // validate проверяет правильность заполнения полей конфигурации.
@@ -78,9 +80,12 @@ func InitConfig() (*Config, error) {
 		WriteTimeout:    baseDurationWriteTimeout,
 		FileStoragePath: "/tmp/short-url-db.json",
 		SecretKey:       "secret",
+		ConfigPath:      "",
 	}
 
 	// Указываем ссылку на переменную, имя флага, значение по умолчанию и описание
+	flag.StringVar(&config.ConfigPath, "c", config.ConfigPath, "Path to configuration file")
+	flag.StringVar(&config.ConfigPath, "config", config.ConfigPath, "Path to configuration file")
 	flag.StringVar(&config.Address, "a", config.Address, "IP-адрес сервера")
 	flag.StringVar(&config.BaseHost, "b", config.BaseHost, "Базовый URL")
 	flag.DurationVar(&config.ReadTimeout, "r", config.ReadTimeout, "ReadTimeout duration")
@@ -94,6 +99,10 @@ func InitConfig() (*Config, error) {
 	flag.Parse()
 
 	// Переопределение значений из переменных окружения, если они заданы
+	if envConfigPath, ok := os.LookupEnv("CONFIG"); ok {
+		config.ConfigPath = envConfigPath
+	}
+
 	if envServAddr, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
 		config.Address = envServAddr
 	}
@@ -137,9 +146,87 @@ func InitConfig() (*Config, error) {
 	}
 
 	if err := config.validate(); err != nil {
-		return &Config{},
-			fmt.Errorf("ошибка валидации конфигурации: %w", err)
+		return &Config{}, fmt.Errorf("ошибка валидации конфигурации: %w", err)
 	}
 
+	// Конфигурационный файл имеет наименьший приоритет
+
+	// Читаем конфиг
+	data, err := os.ReadFile(config.ConfigPath)
+	if err != nil {
+		log.Fatalf("Ошибка при чтении файла: %v", err)
+	}
+
+	// Инициализируем структуру конфигурации
+	var configFromFile map[string]interface{}
+
+	// Декодируем JSON данные в структуру
+	err = json.Unmarshal(data, &configFromFile)
+	if err != nil {
+		log.Fatalf("Ошибка при парсинге JSON: %v", err)
+	}
+
+	// Переносим значения из файла в основную конфигурацию, если они не установлены
+	assignConfigDefaults(config, configFromFile)
+
 	return config, nil
+}
+
+// assignConfigDefaults - функция для переноса значений из второго конфига в основной, если в основном они равны значениям по умолчанию
+func assignConfigDefaults(config *Config, configFromFile map[string]interface{}) {
+	if config.Address == ":8080" {
+		if addr, ok := configFromFile["address"].(string); ok && addr != "" {
+			config.Address = addr
+		}
+	}
+	if config.BaseHost == "http://localhost:8080" {
+		if baseHost, ok := configFromFile["base_host"].(string); ok && baseHost != "" {
+			config.BaseHost = baseHost
+		}
+	}
+	if config.LogLevel == "info" {
+		if logLevel, ok := configFromFile["log_level"].(string); ok && logLevel != "" {
+			config.LogLevel = LogLevel(logLevel)
+		}
+	}
+	if config.ReadTimeout == 10*time.Second {
+		// Пример обработки `read_timeout`, который может быть представлен как строка или число
+		if readTimeoutStr, ok := configFromFile["read_timeout"].(string); ok && readTimeoutStr != "" {
+			// Обработка строки
+			if duration, err := time.ParseDuration(readTimeoutStr); err == nil {
+				config.ReadTimeout = duration
+			}
+		} else if readTimeoutNum, ok := configFromFile["read_timeout"].(float64); ok {
+			// Обработка числа (в секундах)
+			config.ReadTimeout = time.Duration(readTimeoutNum) * time.Second
+		}
+
+	}
+	// Если значение write_timeout в конфигурации по умолчанию
+	if config.WriteTimeout == 10*time.Second {
+		// Обработка, если значение read_timeout представлено как строка
+		if writeTimeoutStr, ok := configFromFile["write_timeout"].(string); ok && writeTimeoutStr != "" {
+			if duration, err := time.ParseDuration(writeTimeoutStr); err == nil {
+				config.WriteTimeout = duration
+			}
+		} else if writeTimeoutNum, ok := configFromFile["write_timeout"].(float64); ok {
+			// Обработка, если значение write_timeout представлено как число
+			config.WriteTimeout = time.Duration(writeTimeoutNum) * time.Second
+		}
+	}
+	if config.FileStoragePath == "/tmp/short-url-db.json" {
+		if fileStoragePath, ok := configFromFile["file_storage_path"].(string); ok && fileStoragePath != "" {
+			config.FileStoragePath = fileStoragePath
+		}
+	}
+	if config.DataBaseDSN == "" {
+		if dataBaseDSN, ok := configFromFile["database_dsn"].(string); ok && dataBaseDSN != "" {
+			config.DataBaseDSN = dataBaseDSN
+		}
+	}
+	if config.SecretKey == "secret" {
+		if secretKey, ok := configFromFile["secret_key"].(string); ok && secretKey != "" {
+			config.SecretKey = secretKey
+		}
+	}
 }
