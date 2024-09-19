8f2eef5d778fd617e752b6f01630927b3f77bbe6
mmskazak <mmskazak@gmail.com>
2024-09-19 12:54:15 +0300

    upd tests

--- internal/storage/infile/user_urls_test.go
+++ internal/storage/infile/user_urls_test.go
@@ -2,6 +2,7 @@ package infile
 
 import (
 	"context"
+	"mmskazak/shorturl/internal/models"
 	"testing"
 
 	"mmskazak/shorturl/internal/storage/inmemory"
@@ -11,7 +12,7 @@ import (
 	"go.uber.org/zap"
 )
 
-func TestInFile_GetUserURLs(t *testing.T) {
+func TestInFile_GetUserURLs_Err(t *testing.T) {
 	ctx := context.Background()
 	userID := "1"
 	baseHost := ""
@@ -31,4 +32,30 @@ func TestInFile_GetUserURLs(t *testing.T) {
 	got, err := m.GetUserURLs(ctx, userID, baseHost)
 	assert.Error(t, err)
 	assert.Nil(t, got)
+}
+
+func TestInFile_GetUserURLs_Success(t *testing.T) {
+	ctx := context.Background()
+	userID := "1"
+	baseHost := "http://localhost"
+	inm, err := inmemory.NewInMemory(zap.NewNop().Sugar())
+	require.NoError(t, err)
+	err = inm.SetShortURL(context.Background(),
+		"short123",
+		"https://example.com",
+		"1",
+		false)
+	require.NoError(t, err)
+	m := &InFile{
+		InMe:     inm,
+		zapLog:   zap.NewNop().Sugar(),
+		filePath: "/mock/path",
+	}
+	got, err := m.GetUserURLs(ctx, userID, baseHost)
+	assert.NoError(t, err)
+	assert.Equal(t, []models.URL{
+		{
+			ShortURL:    "http://localhost/short123",
+			OriginalURL: "https://example.com"},
+	}, got)
 }
