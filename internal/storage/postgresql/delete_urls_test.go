5c7332842acc864c614596710d837e3ddc436b30
mmskazak <mmskazak@gmail.com>
2024-09-19 12:38:26 +0300

    upd tests

--- internal/storage/postgresql/delete_urls_test.go
+++ internal/storage/postgresql/delete_urls_test.go
@@ -3,6 +3,7 @@ package postgresql
 import (
 	"context"
 	"errors"
+	"fmt"
 	"testing"
 
 	"mmskazak/shorturl/internal/storage/postgresql/mocks"
@@ -109,6 +110,31 @@ func TestPostgreSQL_DeleteURLs_CloseError(t *testing.T) {
 		"1234test",
 	}
 
+	s := &PostgreSQL{
+		pool:   mockPool,
+		zapLog: zap.NewNop().Sugar(),
+	}
+	mockPool.On("Begin", ctx).Return(mockTx, nil)
+	mockTx.On("Rollback", ctx).Return(nil)
+	mockTx.On("SendBatch", ctx, mock.Anything).Return(mockBatchResults)
+	mockBatchResults.On("Close").Return(errors.New("test error"))
+
+	err := s.DeleteURLs(ctx, urlIDs)
+	require.Error(t, err)
+	assert.EqualError(t, err, "failed to delete URLs in batch: failed to close mock batch results: test error")
+}
+
+func TestPostgreSQL_DeleteURLs_batchSize5000(t *testing.T) {
+	mockPool := new(mocks.MockDatabase)
+	mockTx := new(mocks.MockTx)
+	mockBatchResults := new(mocks.MockBatchResults)
+	ctx := context.Background()
+
+	urlIDs := make([]string, 0, 5000)
+	for range 5000 {
+		urlIDs = append(urlIDs, "TestTest")
+	}
+	fmt.Println(len(urlIDs))
 	s := &PostgreSQL{
 		pool:   mockPool,
 		zapLog: zap.NewNop().Sugar(),
