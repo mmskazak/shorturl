722534df9bf6a76aac1dd44bcf4f79f3a4c1fe97
mmskazak <mmskazak@gmail.com>
2024-09-29 12:58:04 +0300

    upd tests

---
+++ internal/storage/infile/internal_stats_test.go
@@ -0,0 +1,57 @@
+package infile
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"mmskazak/shorturl/internal/models"
+	"mmskazak/shorturl/internal/storage/inmemory"
+	"testing"
+)
+
+func TestInFile_InternalStatsZeroZero(t *testing.T) {
+	ctx := context.Background()
+	zapLog := zap.NewNop().Sugar()
+	filePath := "/tmp/testpath.json"
+	// Создание экземпляра InMemory
+	inMemory, err := inmemory.NewInMemory(zap.NewNop().Sugar())
+	if err != nil {
+		t.Fatalf("Failed to create InMemory instance: %v", err)
+	}
+
+	f := &InFile{
+		InMe:     inMemory,
+		zapLog:   zapLog,
+		filePath: filePath,
+	}
+	got, err := f.InternalStats(ctx)
+	expected := models.Stats{Urls: "0", Users: "0"}
+	require.NoError(t, err)
+	assert.Equal(t, expected, got)
+}
+
+func TestInFile_InternalStatsTreeTwo(t *testing.T) {
+	ctx := context.Background()
+	zapLog := zap.NewNop().Sugar()
+	filePath := "/tmp/testpath.json"
+	// Создание экземпляра InMemory
+	inMemory, err := inmemory.NewInMemory(zap.NewNop().Sugar())
+	require.NoError(t, err)
+	err = inMemory.SetShortURL(ctx, "TestYanD", "http://ya.ru", "1", false)
+	require.NoError(t, err)
+	err = inMemory.SetShortURL(ctx, "TeStYanD", "http://yandex.ru", "1", false)
+	require.NoError(t, err)
+	err = inMemory.SetShortURL(ctx, "GooGLeeE", "http://google.com", "2", false)
+	require.NoError(t, err)
+
+	f := &InFile{
+		InMe:     inMemory,
+		zapLog:   zapLog,
+		filePath: filePath,
+	}
+	got, err := f.InternalStats(ctx)
+	expected := models.Stats{Urls: "3", Users: "2"}
+	require.NoError(t, err)
+	assert.Equal(t, expected, got)
+}
